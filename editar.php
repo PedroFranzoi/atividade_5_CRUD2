d1f246572c27ddb7134366986b2d7b7537233750
PedroFranzoi <pedro_franzoi@estudante.sesisenai.org.br>
2025-08-11 21:09:54 -0300

    Feita a correção do arquivo editar

--- editar.php
+++ editar.php
@@ -9,17 +9,40 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $sql = "UPDATE usuarios SET nome='$nome', email='$email' WHERE id=$id";
     
+    if ($conn->query($sql) === true) {
+        echo "Registro atualizado com sucesso.
+        <a href='read.php'>Ver registros.</a>
+        ";
+    } else {
+        echo "Erro " . $sql . '<br>' . $conn->error;
+    }
+    $conn->close();
+    exit();
 }
 
 $sql = "SELECT * FROM usuarios WHERE id = $id";
 $res = mysqli_query($conn, $sql);
 $dado = mysqli_fetch_assoc($res);
 
-
 ?>
 
-<form method="POST">
-    Nome: <input type="text" name="nome" value="<?= $dado['nome'] ?>"><br>
-    Email: <input type="email" name="email" value="<?= $dado['email'] ?>"><br>
-    <input type="submit" value="Salvar">
-</form>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>editar</title>
+</head>
+<body>
+    <form method="POST" action="update.php?id=<?php echo $row['id'];?>">
+    <label for="name">Nome:</label>
+        <input type="text" name="name" value="<?php echo $row['name'];?>" required>
+
+        <label for="email">Email:</label>
+        <input type="email" name="email" value="<?php echo $row['email'];?>" required>
+
+        <input type="submit" value="Atualizar">
+    </form>
+</body>
+</html>
+
